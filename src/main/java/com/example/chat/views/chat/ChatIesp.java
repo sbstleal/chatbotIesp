46433de0cfece4341a56a52584707c5652a24541
Sebastião Leal <sebastiaodeoleal@gmail.com>
2022-05-26 23:59:29 -0300

    second commit

--- src/main/java/com/example/chat/views/chat/ChatIesp.java
+++ src/main/java/com/example/chat/views/chat/ChatIesp.java
@@ -5,14 +5,12 @@ import org.alicebot.ab.Chat;
 
 public class ChatIesp extends Chat {
 
-    Teatro teatro = new Teatro();
-
     public ChatIesp(Bot bot) {
         super(bot);
     }
 
     public String multisentenceRespond(String request) {
-        String answer = "";
+        String answer;
 
         if (request.equalsIgnoreCase("menu")) {
             answer = obterMenu();
@@ -21,18 +19,40 @@ public class ChatIesp extends Chat {
             answer = "Acesse nosso site, escolha o metódo para ingressar, e seu curso: " +
                     "https://www.iesp.edu.br/ingresso/inscreva-se";
 
-        } else if (request.contains("2 - Exibir Lugares Ocupados")) {
+        } else if (request.contains("2")) {
 
-            answer = teatro.obterLugaresOcupados();
+            answer = "Acesse o portal do Aluno no site https://sistemas.iesp.edu.br/iesponline/, você já possui o " +
+                    "login? Caso não possua o seu acesso, clique em Primeiro Acesso e crie. Após logar com" +
+                    " sucesso entre na aba Notas e visualize seu desempenho acadêmico.";
 
-        } else if (request.contains("3 - Exibir Lugares Vazios")) {
+        } else if (request.contains("3")) {
 
-            answer = teatro.obterLugaresVazios();
+            answer = "Acesse o portal do Aluno no site https://sistemas.iesp.edu.br/iesponline/, " +
+                    "caso não possua o seu acesso, clique em Primeiro Acesso e crie. Após logar com " +
+                    "sucesso entre na aba" +
+                    " Helpdesk, em seus chamados, e em adicionar você irá criar um novo chamado para " +
+                    "análise da coordenação.";
 
-        } else if (request.split(":").length == 2) {
+        } else if (request.contains("4")) {
 
-            answer = preencherLugar(request);
+            answer = "Acesse o portal do Aluno no site https://sistemas.iesp.edu.br/iesponline/, " +
+                    "caso não possua o seu acesso, clique em Primeiro Acesso e crie. Após logar com " +
+                    "sucesso entre na aba Horário, você visualizará toda seu horário estudantil." +
+                    "Para visualizar o calendário estudantil acesse nosso site: " +
+                    "https://www.iesp.edu.br/noticias/confira-o-calendario-academico-do-semestre-2022-1";
 
+        } else if (request.contains("5")) {
+
+            answer = "A nossa secretária de Sistemas para Internet e de Gestão de Tecnologia da Informação funciona a partir das 13 horas ás 22 horas " +
+                    "e você pode se comunicar através do nosso telefone (83) 2106-3816 | (83) 2106-3879 e " +
+                    "e-mail coordsi@iesp.edu.br";
+
+        } else if (request.contains("6")) {
+
+            answer = "Acesse https://classroom.google.com/u/0/h?hl=pt-BR, seu acesso é número da matrícula@iesp.edu.br, " +
+                    "exemplo: 123456789@iesp.edu.br, e sua senha seu CPF. Aqui você encontrará todas as disciplinas" +
+                    " que você possui, caso não encontre alguma disciplina ou foi incluído em uma disciplina que não identifique em " +
+                    " sua grade curricular, por favor contatar a Coordenação de Sistemas para Internet e de Gestão de Tecnologia da Informação";
         } else {
             return super.multisentenceRespond(request);
         }
@@ -40,18 +60,12 @@ public class ChatIesp extends Chat {
         return answer;
     }
 
-    public String preencherLugar(String request){
-        String[] dadosCadastro = request.split(":");
-        String mensagem = teatro.comprarIngresso(dadosCadastro[0].trim(), Integer.parseInt(dadosCadastro[1].trim()));
-        return  mensagem;
-    }
-
     private String obterMenu(){
-        String menu  = "1 - Faça sua Matrícula";
+        String menu  = " 1 - Faça sua Matrícula";
         menu += " 2 - Verifique seu Desempenho Acadêmico";
-        menu += " 3 - Nunca tive contato com Tecnologia, posso iniciar um curso?";
-        menu += " 4 - Qual o seu Horário e Calendário Estudantil";
-        menu += " 5 - Verifique se possuímos vagas de Estágio";
+        menu += " 3 - Como faço para solicitar dispensa das disciplinas?";
+        menu += " 4 - Qual o seu Horário e Calendário Estudantil?";
+        menu += " 5 - Qual horário que consigo fala com a coordenação?";
         menu += " 6 - Como acessar a nossa Plataforma de Estudo? ";
 
         return menu;
